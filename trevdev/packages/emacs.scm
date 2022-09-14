6c4369665e8400de53151b6491ac2f4bd505c550
Trevor Richards <trev@trevdev.ca>
2022-09-14 11:18:10 -0700

    packages: added emacs-org-roam-ui

--- trevdev/packages/emacs.scm
+++ trevdev/packages/emacs.scm
@@ -8,7 +8,8 @@
   #:use-module (gnu packages emacs-xyz)
   #:use-module (gnu packages mail)
   #:export (emacs-ol-notmuch
-            emacs-lsp-mode-with-plists))
+            emacs-lsp-mode-with-plists
+            emacs-org-roam-ui))
 
 (define emacs-ol-notmuch
   (let ((commit "1a53d6c707514784cabf33d865b577bf77f45913")
@@ -49,3 +50,31 @@ by a query, so both a link can prefer to several mails.")
            (add-after 'unpack 'setenv-plists-true
                (lambda _
                  (setenv "LSP_USE_PLISTS" "true")))))))))
+
+(define emacs-org-roam-ui
+  (let ((commit "16a8da9e5107833032893bc4c0680b368ac423ac")
+        (revision "0"))
+    (package
+      (name "emacs-org-roam-ui")
+      (version (git-version "0.1.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/org-roam/org-roam-ui.git")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "0l5cbd0al5idc9pckl4885vp3449awvz5sgz0r998say5xxsajii"))))
+      (build-system emacs-build-system)
+      (propagated-inputs (list emacs-org-roam
+                               emacs-simple-httpd
+                               emacs-websocket))
+      (home-page "https://github.com/org-roam/org-roam-ui")
+      (synopsis "A frontend for exploring your org-roam notes.")
+      (description "Org-Roam-UI is a frontend for exploring and interacting
+with your org-roam notes.
+
+Org-Roam-UI is meant a successor of org-roam-server that extends functionality
+ of org-roam with a Web app that runs side-by-side with Emacs.")
+      (license license:gpl3))))
