664c8901f31454749a11c500ebd6d58f9384b9a0
Trevor Richards <trev@trevdev.ca>
2022-09-19 21:19:08 -0700

    packages: sbcl-stumpwm-pamixer: Improve package input support

--- trevdev/packages/stumpwm.scm
+++ trevdev/packages/stumpwm.scm
@@ -16,31 +16,32 @@
 
 (define sbcl-stumpwm-pamixer
   (let ((commit "aa820533c80ea1af5a0e107cea25eaf34e69dc24")
-        (revision "4"))
+        (revision "5"))
     (package
       (name "sbcl-stumpwm-pamixer")
       (version (git-version "0.1.1" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/Junker/stumpwm-pamixer")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "0djcrr16bx40l7b60d4j507vk5l42fdgmjpgrnk86z1ba8wlqim8"))))
-      (inputs `(("stumpwm:lib" ,stumpwm "lib")
-                ("pamixer" ,pamixer)))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/Junker/stumpwm-pamixer")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "0djcrr16bx40l7b60d4j507vk5l42fdgmjpgrnk86z1ba8wlqim8"))))
+      (inputs (list `(,stumpwm "lib") pamixer))
       (build-system asdf-build-system/sbcl)
       (arguments
-       `(#:asd-systems '(:pamixer)
-         #:phases
-         ,#~(modify-phases %standard-phases
-             (add-after 'unpack 'patch-pamixer-path
-               (lambda _
-                 (substitute* "pamixer.lisp"
-                   (("\"pamixer ")
-                    (string-append "\"" #$pamixer "/bin/pamixer "))))))))
+       (list #:asd-systems ''(:pamixer)
+             #:phases #~(modify-phases %standard-phases
+                          (add-after 'unpack 'patch-pamixer
+                            (lambda _
+                              (substitute* "pamixer.lisp"
+                                (("\"pamixer \"")
+                                 (string-append "\""
+                                                #$(this-package-input
+                                                   "pamixer")
+                                                "/bin/pamixer \""))))))))
       (home-page "https://github.com/Junker/stumpwm-pamixer")
       (synopsis "StumpWM Pamixer Module")
       (description "Minimalistic Pulseaudio volume and microphone control
